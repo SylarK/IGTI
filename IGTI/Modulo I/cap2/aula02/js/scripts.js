0000bc61d40d5fd47329e0281c41f9ae865ec8f7
SylarK <syst.lucasamado@gmail.com>
2020-09-18 17:38:30 -0300

    Intro Javascript

---
+++ IGTI/Modulo I/cap2/aula02/js/scripts.js
@@ -0,0 +1,34 @@
+/*
+  Tipos Javascript
+  . Number
+  . String
+  . Boolean
+  . Null
+  . Undefined
+  . Object
+
+  - Linguagem de programação com tipagem fraca
+
+  false, 0, null, undefined => Falsy
+  true, 1, [1] => Truth
+
+  - Comparação
+  . ===
+  . !==
+  . >
+  . <
+  . >=
+  . <=
+
+  . &&
+  . ||
+  . !
+
+  - Operadores
+  . +
+  . -
+  . /
+  . *
+  . %
+
+  */
