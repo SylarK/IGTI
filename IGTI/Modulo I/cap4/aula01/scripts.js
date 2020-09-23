162240f71e38ff2cae73303980da1f34cd9d4401
unknown <lucasamadort@gmail.com>
2020-09-23 08:24:53 -0300

    Entendendo variáveis no ES+, template literals e default parameters

---
+++ IGTI/Modulo I/cap4/aula01/scripts.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/*
+  VARIÁVEIS
+  var -> possui escopo amplo / abrangente
+  let -> possui escopo reduzido
+  const -> possui valor imutável (não se pode retribuir valores)
+
+*/
+
+/*
+  FUNCTIONS
+
+  - Normal
+  function sum(a, b){
+    return a + b;
+  }
+ 
+  - Anônima
+  const sum2 = function(a,b){
+    return a + b;
+  }
+
+  - Arrow Function
+  const sum3 = (a,b) => {
+    return a + b;
+  }
+
+  const sum4 = (a,b) => a + b;
+
+*/
---
+++ IGTI/Modulo I/cap4/aula02/js/scripts.js
@@ -0,0 +1,13 @@
+'strict use';
+
+/*
+  PRINCIPAIS MÉTODOS ARRAYS
+  - map -> gera um novo array transformando os dados
+  - filter -> gera um novo array filtrando os elementos
+  - forEach -> percorre o array
+  - reduce -> cálculo interativo com base nos elementos
+  - find -> encontra elementos com base em proposições
+  - some -> verifica se existe pelo menos um elemento que atenda à preposição
+  - every -> verifica se todos os elementos atendem à preposição
+  - sort -> ordena os elementos com base em um critério
+*/
