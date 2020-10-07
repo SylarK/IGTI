5dd1e1ad6d9062c16b5c061491c969c7ffff3aea
SylarK <syst.lucasamado@gmail.com>
2020-10-07 19:11:53 -0300

    Alt.

--- IGTI/Modulo II/desafio01/index.js
+++ IGTI/Modulo II/desafio01/index.js
@@ -72,11 +72,41 @@ async function question03(){
         for(let i = 0; i < dadosEstados.length; i++){
             //data[dadosEstados][i]['Sigla'] = question02(dadosEstados[i]['Sigla'])
             //console.log(dadosEstados[i]['Sigla']);
-            const estado = JSON.parse
+            const estado = JSON.parse(await fs.readFile('estados/' + dadosEstados[i]['Sigla'] + '.json'));
+            //console.log(estado[dadosEstados[i]['Sigla']].length);
+            //res = [{sigla: dadosEstados[i]['Sigla'], tamanho: estado[dadosEstados[i]['Sigla']].length}]
+            data[dadosEstados[i]['Sigla']] = estado[dadosEstados[i]['Sigla']].length;
         }
-    
-        //console.log(data);
+        
+        
+        const order = Object.fromEntries(
+            Object.entries(data).sort(([,a],[,b]) => a-b)
+        );
+        
+        console.log(order);
+
     }catch(err){
         getError(err);
     }
 }
+
+/*
+var maxSpeed = {
+    car: 300, 
+    bike: 60, 
+    motorbike: 200, 
+    airplane: 1000,
+    helicopter: 400, 
+    rocket: 8 * 60 * 60
+};
+var sortable = [];
+for (var vehicle in maxSpeed) {
+    sortable.push([vehicle, maxSpeed[vehicle]]);
+}
+
+sortable.sort(function(a, b) {
+    return a[1] - b[1];
+});
+
+
+*/
