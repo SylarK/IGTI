1aba3afc593ea2cc183f59caf9c57bbe4f784ba1
SylarK <syst.lucasamado@gmail.com>
2020-10-11 10:50:55 -0300

    Criacao de API (GET, POST, PUT, PATCH, DELETE) e gravacao de logs

--- IGTI/Modulo II/cap5/index.js
+++ IGTI/Modulo II/cap5/index.js
@@ -1,26 +1,48 @@
 import express from 'express';
+import winston from 'winston';
+import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
 import accountsRouter from './routes/accounts.js';
 import { promises as fs } from 'fs';
+import { swaggerDocument } from './doc.js';
 
 const { readFile, writeFile } = fs;
 
+global.filename = 'accounts.json';
+
+const { combine, timestamp, label, printf } = winston.format;
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  return `${timestamp} [${label}] ${level}: ${message}`;
+});
+global.logger = winston.createLogger({
+  level: 'silly',
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: 'my-bank-api.log ' }),
+  ],
+  format: combine(label({ label: 'my-bank-api ' }), timestamp(), myFormat),
+});
+
 const app = express();
 app.use(express.json());
+app.use(cors());
+app.use(express.static('public'));
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use('/account', accountsRouter);
 
 app.listen(3000, async () => {
   try {
-    await readFile('accounts.json');
-    console.log('API Started...');
+    await readFile(global.filename);
+    logger.info('API Started...');
   } catch (err) {
     const initialJson = { nextId: 1, accounts: [] };
-    writeFile('accounts.json', JSON.stringify(initialJson))
+    writeFile(global.filename, JSON.stringify(initialJson))
       .then(() => {
-        console.log('API Started...');
+        logger.info('API Started...');
       })
       .catch((err) => {
-        console.log('Something is wrong! Check => ' + err);
+        logger.error('Something is wrong! Check => ' + err);
       });
   }
 });
--- IGTI/Modulo II/cap5/routes/accounts.js
+++ IGTI/Modulo II/cap5/routes/accounts.js
@@ -1,25 +1,109 @@
 import express from 'express';
+//import cors from 'cors';
 import { promises as fs } from 'fs';
 
 const { readFile, writeFile } = fs;
 
 const router = express.Router();
 
-router.post('/', async (req, res) => {
+router.post('/', async (req, res, next) => {
   try {
     let account = req.body;
-    const data = JSON.parse(await readFile('accounts.json'));
 
-    account = { id: data.nextId++, ...account };
+    if (!account.nome || account.balance == null) {
+      throw new Error('Name e Balance são obrigatórios! ');
+    }
+
+    const data = JSON.parse(await readFile(global.filename));
+
+    account = {
+      id: data.nextId++,
+      name: account.nome,
+      balance: account.balance,
+    };
     //data.nextId++;
     data.accounts.push(account);
 
-    await writeFile('accounts.json', JSON.stringify(data, null, 2));
+    await writeFile(global.filename, JSON.stringify(data, null, 2));
 
     res.send(account);
+
+    logger.info(` POST / account - ${JSON.stringify(account)}`);
   } catch (err) {
-    res.status(400).send({ error: err.message });
+    next(err);
   }
+});
+//router.get('/', cors(), async (req, res, next)
+router.get('/', async (req, res, next) => {
+  try {
+    const data = JSON.parse(await readFile(global.filename));
+    delete data.nextId;
+    res.send(data);
+    logger.info(' GET / account');
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const data = JSON.parse(await readFile(global.filename));
+    const account = data.accounts.find(
+      (account) => account.id === parseInt(req.params.id)
+    );
+    res.send(account);
+    logger.info(' GET / account/:id');
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const data = JSON.parse(await readFile(global.filename));
+    data.accounts = data.accounts.filter(
+      (account) => account.id !== parseInt(req.params.id)
+    );
+
+    await writeFile(global.filename, JSON.stringify(data, null, 2));
+
+    res.end();
+    logger.info(' DELETE / account/:id - ' + req.params.id);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.put('/', async (req, res, next) => {
+  try {
+    const account = req.body;
+
+    if (!account.nome || account.balance == null) {
+      throw new Error('Name e Balance são obrigatórios! ');
+    }
+
+    const data = JSON.parse(await readFile(global.filename));
+    const index = data.accounts.findIndex((a) => a.id === account.id);
+
+    if (index === -1) {
+      throw new Error('Registro não encontrado.');
+    }
+
+    data.accounts[index].nome = account;
+    data.accounts[index].balance = account;
+
+    await writeFile(global.filename, JSON.stringify(data, null, 2));
+
+    res.send(account);
+    logger.info(` PUT / account - ${JSON.stringify(account)}`);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.use((err, req, res, next) => {
+  global.logger.error(`${err.message}`);
+  res.status(400).send({ error: err.message });
 });
 
 export default router;
