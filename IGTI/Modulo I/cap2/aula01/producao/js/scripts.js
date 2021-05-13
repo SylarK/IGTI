6cd6d579643432bd4fdc8d31436ef5d422ea5633
SylarK <syst.lucasamado@gmail.com>
2021-05-13 06:46:52 -0300

    Alt

--- IGTI/Modulo I/cap2/aula01/producao/js/scripts.js
+++
@@ -1,4 +0,0 @@
-console.log('Hello, World!');
-
-var title = document.querySelector('h1');
-title.textContent = 'Hey Guys';
--- IGTI/Modulo II/cap2/Events/index.js
+++
@@ -1,7 +0,0 @@
-import ev from './events.js';
-
-ev.on('testEvent', () => {
-    console.log('Estou te ouvindo também.');
-})
-
-ev.emit('testEvent', 'You are listen me.');
